03078e7d726f5ba9c539d91744c66a3c43f5b26d
ITZSHOAIB <skshoaibpersonal@gmail.com>
2023-11-13 22:52:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function () {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.getElementById(button.dataset.id).scrollIntoView();
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
